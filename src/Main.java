87584bd941d1f68b458cb9e84301307dc97f63a9
Sebastian Norén <s.noren89@gmail.com>
2019-04-10 10:29:09 +0200

    Base

--- src/Main.java
+++ src/Main.java
@@ -2,20 +2,26 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
+
+
 
 public class Main extends Application {
 
     private static Stage window;
+    private static final String SOFTWARE_NAME = "PUST GIS";
 
     @Override
     public void start(Stage primaryStage) throws Exception {
 
         window = primaryStage;
+        Image image = new Image(getClass().getResourceAsStream("/logo2c.png"));
+        primaryStage.getIcons().add(image);
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        window.setTitle("Police System");
+        window.setTitle(SOFTWARE_NAME);
         Scene scene = new Scene(root);
-        //scene.getStylesheets().add("riper.css");
+        //scene.getStylesheets().add("xxxx.css");
         window.setScene(scene);
         window.show();
     }
