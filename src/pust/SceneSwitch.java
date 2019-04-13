552cde6948327f4a16144356748b968f2337b0d8
Sebastian Norén <s.noren89@gmail.com>
2019-04-13 18:43:17 +0200

    Update AdminCreation

--- src/pust/SceneSwitch.java
+++ src/pust/SceneSwitch.java
@@ -16,10 +16,25 @@ Model for switch scenes
 
 public class SceneSwitch {
 
-    void switchS(Event event) {
+    void goToAdminCreate(Event event) {
         try {
             Stage primaryStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
             Parent root = FXMLLoader.load(getClass().getResource("/pust/adminCreate/Admins.fxml"));
+            Image image = new Image(getClass().getResourceAsStream("/swepustlogg.png"));
+            primaryStage.getIcons().add(image);
+            Scene scene = new Scene(root);
+            primaryStage.setScene(scene);
+            primaryStage.setTitle(AppConstant.SOFTWARE_NAME);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+   public void goToLogin(Event event) {
+        try {
+            Stage primaryStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            Parent root = FXMLLoader.load(getClass().getResource("/pust/LogInScreen.fxml"));
             Image image = new Image(getClass().getResourceAsStream("/swepustlogg.png"));
             primaryStage.getIcons().add(image);
             Scene scene = new Scene(root);
