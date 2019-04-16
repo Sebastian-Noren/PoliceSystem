0702eebc4049ffa4e70e201de8d9c385eeeac30b
Soutine <jsoutine@gmail.com>
2019-04-16 09:22:31 +0200

    refined log in screen and added some methods

--- src/pust/LogInController.java
+++ src/pust/LogInController.java
@@ -6,6 +6,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 import java.net.URL;
@@ -20,14 +21,23 @@ public class LogInController implements Initializable {
     @FXML
     TextField passWord;
     @FXML
+    Label passWarning;
+    @FXML
+    Label passForgot;
+    @FXML
     Button logInBtn;
     DatabaseConnection database;
     boolean connected;
 
+    int counter;
+
     public void logInBtn(ActionEvent actionEvent) {
+        counter++;
 
         database = new DatabaseConnection();
         connected = database.Loginconnect(userName.getText(), passWord.getText());
+
+        passwordCounter(counter);
 
         //TODO make strings safe for root Admin. Sebastians shit
         if (connected && userName.getText().equals("root") && userName.getText().equals("root")) {
@@ -38,10 +48,21 @@ public class LogInController implements Initializable {
         }
     }
 
+    public void passwordCounter(int wrongPass){
+        if (wrongPass >0 && wrongPass < 3){
+            passWarning.setText("Incorrect username or password");
+        } else if (wrongPass >= 3) {
+            passWarning.setText("YOU'VE BEEN LOCKED OUT BITCH");
+            //create method to lock out
+        }
+    }
+
+
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
         Platform.runLater( () -> logInBtn.requestFocus() );
+        passWarning.setText("");
     }
 }
 
