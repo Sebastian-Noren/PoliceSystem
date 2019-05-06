c5093f7670c71ca0c260a5a226801be9a0850d7a
Ali Muhammed <Alawi93@live.se>
2019-05-06 11:26:03 +0200

    passport

--- src/main/java/pust/controller/main_window/ApplyForPassportController.java
+++ src/main/java/pust/controller/main_window/ApplyForPassportController.java
@@ -1,21 +1,361 @@
 package pust.controller.main_window;
 
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.FileChooser;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URL;
+import java.security.SecureRandom;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
-public class ApplyForPassportController {
+public class ApplyForPassportController implements Initializable {
+    @FXML
+    private Button backBtn;
+    @FXML
+    private AnchorPane anchorPane;
+    @FXML
+    private Button nextBtn, uppload;
+    //will be autofilled later
+    @FXML
+    private TextField firstName, lastName, dateOfBirth, sex, placeOfBirth, ssn, height, hairColor, eyeColor, weight;
+    //auto filled
+    @FXML
+    private TextField nationality, type, code, dateOfIssue, dateOfExpiry, authority, passportNbr;
+    @FXML
+    private ImageView iconImage;
+
+    //upload image
+    private Image[] upploadImage = new Image[1];
+    //place of birth
+    private String[] birthPlace = new String[10];
 
 
-    public void uploadImage() {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        SecureRandom secureRandom = new SecureRandom();
+
+        //fill array with places
+        automaticBirthPlace();
+        //set random place
+        placeOfBirth.setText(birthPlace[secureRandom.nextInt(birthPlace.length)]);
+
+        Image image = new Image("image/swepustlogg.png");
+        iconImage.setImage(image);
+
+        nationality.setText("SWEDISH");
+        type.setText("P");
+        code.setText("SWE");
+
+
+        //get current date
+        Calendar currentDate = Calendar.getInstance();
+        //today's date
+        Date date = new Date();
+        currentDate.setTime(date);
+        //format we show it as
+        DateFormat dateFormat = new SimpleDateFormat("YYYY/MM/dd");
+        //set the current date
+        dateOfIssue.setText(dateFormat.format(date.getTime()));
+
+        currentDate.add(Calendar.YEAR, 5);
+        //we get the date + 5 years we added
+        dateOfExpiry.setText(dateFormat.format(currentDate.getTime()));
+        //set authority text
+        authority.setText("P.G.I.S");
+
+        //generate random passport number
+        int numberFrom = 1000000;
+        int numberTo = 100000000;
+        int randomnbr = secureRandom.nextInt(numberTo - numberFrom) + numberFrom;
+
+        passportNbr.setText(String.valueOf(randomnbr));
+
+        //TODO fix
+        //When we auto fill ssn then we will activate the method
+        //but for now we need to type in the information manually.
+        ssn.setOnKeyPressed(e -> {
+            automaticDateOfBirth();
+            automaticGender();
+
+        });
+
+        uppload.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                upploadImage();
+            }
+        });
+
+
+        //set back button on action back();
+        backBtn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                back();
+
+            }
+        });
+
+
+        nextBtn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+
+                next();
+
+            }
+        });
+
+    }
+
+    public void back() {
+        try {
+            //Set fxml (scene 2) to AnchorPane pane
+            AnchorPane pane = FXMLLoader.load(getClass().getResource("/view/main_window/Passport.fxml"));
+            //insert pane to current anchorPane
+            anchorPane.getChildren().setAll(pane);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+
+        }
+    }
+
+    public void upploadImage() {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/main_window/PassportFinished.fxml"));
+
+        //upload image
         FileChooser fileChooser = new FileChooser();
         File selectedImage = fileChooser.showOpenDialog(null);
+
         List<File> uploadedImage = new ArrayList<>();
         uploadedImage.add(selectedImage);
 
+
+        try {
+            FileInputStream fileInputStream = new FileInputStream(uploadedImage.get(0));
+            Image image1 = new Image(fileInputStream, 160, 194, false, false);
+            //insert uploaded image to array
+            upploadImage[0] = image1;
+
+
+            //get uploaded file
+           switch(uploadedImage.get(0).getName()){
+               case "woman1.jpg":
+                   this.height.setText("164");
+                   this.hairColor.setText("Light brown");
+                   this.eyeColor.setText("Green");
+                   this.weight.setText("62");
+                   break;
+               case "man1.jpg":
+                   this.height.setText("174");
+                   this.hairColor.setText("Black");
+                   this.eyeColor.setText("Brown");
+                   this.weight.setText("77");
+                   break;
+               case "woman2.jpg":
+                   this.height.setText("170");
+                   this.hairColor.setText("Blond");
+                   this.eyeColor.setText("Blue");
+                   this.weight.setText("69");
+                   break;
+               case "man2.jpg":
+                   this.height.setText("178");
+                   this.hairColor.setText("Brown/Grey");
+                   this.eyeColor.setText("Light brown/Yellow");
+                   this.weight.setText("78");
+                   break;
+               case "woman3.jpg":
+                   this.height.setText("174");
+                   this.hairColor.setText("Brown");
+                   this.eyeColor.setText("Light brown/Grey");
+                   this.weight.setText("73");
+                   break;
+                   default:
+                   System.out.println("outside the fold of automatic");
+                   break;
+
+
+           }
+
+
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
+
+
+    public void next() {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/main_window/PassportFinished.fxml"));
+            AnchorPane pane = loader.load();
+            anchorPane.getChildren().setAll(pane);
+
+            PassportFinishedController passportFinishedController = loader.getController();
+            //sending information to next scene
+            passportFinishedController.setText(firstName.getText(), lastName.getText(), dateOfBirth.getText(), sex.getText(), placeOfBirth.getText(), ssn.getText(), height.getText(),
+                    hairColor.getText(), eyeColor.getText(), weight.getText(), nationality.getText(), type.getText(), code.getText(), dateOfIssue.getText(), dateOfExpiry.getText(),
+                    authority.getText(), passportNbr.getText());
+
+            passportFinishedController.setProfileImage(upploadImage[0]);
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+
+        }
+
+    }
+
+    public void automaticBirthPlace() {
+        String place = "LUND";
+        birthPlace[0] = place;
+        String place1 = "STOCKHOLM";
+        birthPlace[1] = place1;
+        String place2 = "KRISTIANSTAD";
+        birthPlace[2] = place2;
+        String place3 = "ESLÖV";
+        birthPlace[3] = place3;
+        String place4 = "HÖRBY";
+        birthPlace[4] = place4;
+        String place5 = "MALMÖ";
+        birthPlace[5] = place5;
+        String place6 = "HÄSSLEHOLM";
+        birthPlace[6] = place6;
+        String place7 = "BÅSTAD";
+        birthPlace[7] = place7;
+        String place8 = "HELSINGBORG";
+        birthPlace[8] = place8;
+        String place9 = "TRELLEBORG";
+        birthPlace[9] = place9;
+
+    }
+    public void automaticGender(){
+        String gender = this.ssn.getText();
+        char gender1 = gender.charAt(10);
+
+        boolean even = (gender1%2) ==0? true:false;
+
+        //if true (true that u can divide the number with 2) then its a even number = gender woman else man (odd number)
+        if (even){
+            sex.setText("W");
+        }else{
+            sex.setText("M");
+        }
+    }
+
+    //set date of birth based on ssn entered!
+    public void automaticDateOfBirth() {
+
+
+        //take ssn and add it to string
+        String ssn = this.ssn.getText();
+        //get day
+        char day = ssn.charAt(6);
+        char day1 = ssn.charAt(7);
+        //take specific indexes and create a string (stringBuilder)
+        StringBuilder day2 = new StringBuilder();
+        day2.append(day);
+        day2.append(day1);
+        //set our created string (stringBuilder) to a string
+        String day3 = day2.toString();
+
+
+        //cut character at specific index of ssn and create a char of that index [cut at the month]
+        char month = ssn.charAt(4);
+        char month1 = ssn.charAt(5);
+
+        //take specific indexes and create a string
+        StringBuilder month3 = new StringBuilder();
+        month3.append(month);
+        month3.append(month1);
+
+        //set our created string (stringBuilder) to a string
+        String month4 = month3.toString();
+
+        System.out.println(month3);
+        // for specific month
+
+
+        switch (month4) {
+            case "01":
+                this.dateOfBirth.setText(day3 + " JANUARY");
+                break;
+            case "02":
+                this.dateOfBirth.setText(day3 + " FEBRUARY");
+                break;
+            case "03":
+                this.dateOfBirth.setText(day3 + " MARCH");
+                break;
+            case "04":
+                this.dateOfBirth.setText(day3 + " APRIL");
+                break;
+            case "05":
+                this.dateOfBirth.setText(day3 + " MAY");
+                break;
+            case "06":
+                this.dateOfBirth.setText(day3 + " JUNE");
+                break;
+            case "07":
+                this.dateOfBirth.setText(day3 + " JULY");
+                break;
+            case "08":
+                this.dateOfBirth.setText(day3 + " AUGUST");
+                break;
+            case "09":
+                this.dateOfBirth.setText(day3 + " SEPTEMBER");
+                break;
+            case "10":
+                this.dateOfBirth.setText(day3 + " OCTOBER");
+                break;
+            case "11":
+                this.dateOfBirth.setText(day3 + " NOVEMBER");
+                break;
+            case "12":
+                this.dateOfBirth.setText(day3 + " DECEMBER");
+                break;
+            default:
+                this.dateOfBirth.setText("NOT VALID!");
+                break;
+
+
+        }
+
+        //year mon day
+        //1993 03 03 7353
+        /*
+        01 = January
+        02 = February
+        03 = March
+        04 = April
+        05 = May
+        06 = June
+        07 = July
+        08 = August
+        09 = September
+        10 = October
+        11 = November
+        12 = December
+         */
+
+
+    }
+
 
 }
---
+++ src/main/java/pust/controller/main_window/PassportFinishedController.java
@@ -0,0 +1,213 @@
+package pust.controller.main_window;
+
+import javafx.embed.swing.SwingFXUtils;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.geometry.Bounds;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.effect.GaussianBlur;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class PassportFinishedController implements Initializable {
+
+
+    @FXML
+    private AnchorPane anchorPane;
+    @FXML
+    public ImageView profileImg, profileImg1, profileImage2;
+    @FXML
+    private ImageView imageViewpass;
+    @FXML
+    private Button backBtn;
+    @FXML
+    public Button screenshot;
+    //user fill in
+    @FXML
+    private Label name, dateOfBirth, sex, placeOfBirth, ssn, height, hairColor, eyeColor, weight;
+
+    //labels that are autofilled
+    @FXML
+    private Label nationality, type, code, dateOfIssue, dateOfExpiry, signature, passportNbr, authority;
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+
+        screenshot.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                screenShot();
+            }
+        });
+
+        backBtn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                back();
+            }
+        });
+
+
+    }
+
+
+    public void setText(String firstname, String lastname, String dateOfBirth, String sex, String placeOfBirth, String ssn, String height,
+                        String hairColor, String eyeColor, String weight, String nationality, String type, String code, String dateOfIssue, String dateOfExpiry,
+                        String authority, String passportNbr) {
+
+        //Standard text for information
+        Font font = Font.font("TimesRoman", FontWeight.THIN, 15);
+        //for nationality,Code,Type
+        Font font1 = Font.font("Verdana", FontWeight.BOLD, 15);
+        //for signature
+        Font font2 = Font.font("Serif", FontWeight.EXTRA_LIGHT, 20);
+
+        //labels that users fill out (will later change to autofill but these labels will still have this logic)
+        String fullname = firstname + " " + lastname;
+        this.name.setFont(font);
+        this.name.setText(fullname);
+
+        this.dateOfBirth.setFont(font);
+        this.dateOfBirth.setText(dateOfBirth);
+
+        this.sex.setFont(font);
+        this.sex.setText(sex);
+
+        this.placeOfBirth.setFont(font);
+        this.placeOfBirth.setText(placeOfBirth);
+
+        this.ssn.setFont(font2);
+        this.ssn.setText(ssn);
+
+        this.height.setFont(font);
+        this.height.setText(height + " " + "cm");
+
+        this.hairColor.setFont(font);
+        this.hairColor.setText(hairColor);
+
+        this.eyeColor.setFont(font);
+        this.eyeColor.setText(eyeColor);
+
+        this.weight.setFont(font);
+        this.weight.setText(weight + " " + "kg");
+
+        //labels that are autoFilled
+
+        //set the font
+        this.nationality.setFont(font1);
+        //set text colour
+        this.nationality.setTextFill(Color.PURPLE);
+        //set text
+        this.nationality.setText(nationality);
+
+        //set the font
+        this.type.setFont(font1);
+        //set text colour
+        this.type.setTextFill(Color.PURPLE);
+        //set text
+        this.type.setText(type);
+
+        //set the font
+        this.code.setFont(font1);
+        //set text colour
+        this.code.setTextFill(Color.PURPLE);
+        //set text
+        this.code.setText(code);
+
+        //set the font
+        this.dateOfIssue.setFont(font);
+        //set the current date
+        this.dateOfIssue.setText(dateOfIssue);
+        //set the text (current date + 5 years)
+        this.dateOfExpiry.setText(dateOfExpiry);
+
+        //work in progress to make the text realistic if needed
+        //set the font
+        signature.setFont(font2);
+        //get the text
+        signature.setText(name.getText());
+
+        //Set the font
+        this.authority.setFont(font1);
+        //set the text
+        this.authority.setText(authority);
+        //set the font
+        this.passportNbr.setFont(font);
+        //set the random number
+        this.passportNbr.setText(passportNbr);
+    }
+
+    public void back() {
+        try {
+
+            AnchorPane pane = FXMLLoader.load(getClass().getResource("/view/main_window/applyForPassport.fxml"));
+            //insert pane to current anchorPane
+            anchorPane.getChildren().setAll(pane);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+
+        }
+    }
+
+
+    public void setProfileImage(Image image) {
+        profileImg.setImage(image);
+
+        GaussianBlur gaussianBlur = new GaussianBlur();
+
+        profileImage2.setEffect(gaussianBlur);
+        profileImage2.setImage(image);
+
+
+        profileImg1.setImage(image);
+    }
+
+    public void screenShot() {
+        try {
+
+            Robot robot = new Robot();
+            //get node location relative to the screen
+            Bounds bounds = imageViewpass.localToScreen(imageViewpass.getBoundsInLocal());
+            //using node location to define where a rectangle will be placed on the screen and define the node width and height
+            Rectangle rect = new Rectangle((int) bounds.getMinX(), (int) bounds.getMinY(), (int) imageViewpass.getFitWidth(), (int) imageViewpass.getFitHeight());
+
+            BufferedImage image = robot.createScreenCapture(rect);
+
+            Image myCaptured = SwingFXUtils.toFXImage(image, null);
+
+            ImageIO.write(image, "jpg", new File("C:\\Users\\Alawi\\Desktop\\" + ssn.getText() + ".jpg"));
+
+            imageViewpass.setImage(myCaptured);
+
+        } catch (AWTException e) {
+            e.printStackTrace();
+            // }catch(IOException e){
+
+        } catch (IOException e) {
+
+        }
+
+
+    }
+
+
+}
