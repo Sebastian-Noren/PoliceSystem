0e3367f157369b84344190dfbaef48dbc7ce78e9
Sebastian Norén <s.noren89@gmail.com>
2019-05-21 21:07:51 +0200

    Linking code together, Unity in program.

--- src/main/java/pust/controller/main_window/ViewWantedController.java
+++ src/main/java/pust/controller/main_window/ViewWantedController.java
@@ -11,6 +11,7 @@ import pust.model.database_functionality.SelectPerson;
 import pust.model.entity.Person;
 import pust.model.entity.Suspect;
 import pust.model.utility.AppConstant;
+
 import java.net.URL;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
@@ -21,7 +22,7 @@ public class ViewWantedController implements Initializable {
 
     @FXML
     private Label labelHeadName, labelWanted, labelAlias, labelSSN, labelDateBirth, labelGender,
-            labelEthnicity, labelHair, labelHeight, labelWeight, labelEyes, labelThin, labelDescription,labelCurrentDate;
+            labelEthnicity, labelHair, labelHeight, labelWeight, labelEyes, labelThin, labelDescription, labelCurrentDate;
     @FXML
     private ListView listWanted;
     private Suspect suspect = null;
@@ -32,34 +33,39 @@ public class ViewWantedController implements Initializable {
         LocalDate date = LocalDate.now();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
         labelCurrentDate.setText(String.format("Last Updated: %s", date.format(formatter)));
+        if (AppConstant.person.isWanted()) {
+            WantedDatabase wantedDatabase = new WantedDatabase();
+            String crime = String.format("Wanted for %s.", wantedDatabase.getWantedCrime(AppConstant.person.getPersonalNumber().getPersonalNumber()));
+            setlabel(AppConstant.person, AppConstant.suspect, crime);
+        }
     }
 
     @FXML
     private void clickSuspectWanted() {
-            String prepeareString = "";
-            ObservableList<String> suspect = listWanted.getSelectionModel().getSelectedItems();
-            for (String m : suspect) {
-                prepeareString = m;
-            }
-            String splitSSN = "";
-            String[] parts = prepeareString.split(("\\D"));
-            for (String theParts : parts) {
-                splitSSN = theParts;
-            }
-            getSuspectedFromList(splitSSN);
+        String prepeareString = "";
+        ObservableList<String> suspect = listWanted.getSelectionModel().getSelectedItems();
+        for (String m : suspect) {
+            prepeareString = m;
+        }
+        String splitSSN = "";
+        String[] parts = prepeareString.split(("\\D"));
+        for (String theParts : parts) {
+            splitSSN = theParts;
+        }
+        getSuspectedFromList(splitSSN);
     }
 
-    private void getSuspectedFromList(String splitSSN){
+    private void getSuspectedFromList(String splitSSN) {
         WantedDatabase wantedDatabase = new WantedDatabase();
         Person person = new SelectPerson(splitSSN).loadPerson();
         if (person instanceof Suspect) {
             suspect = (Suspect) person;
         }
         String crime = String.format("Wanted for %s.", wantedDatabase.getWantedCrime(splitSSN));
-        setlabel(person, suspect,crime);
+        setlabel(person, suspect, crime);
     }
 
-    private void updateWantedlist(){
+    private void updateWantedlist() {
         WantedDatabase wantedDatabase = new WantedDatabase();
         listWanted.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
         ArrayList<String> listOfWantedSuspects = wantedDatabase.getSuspects();
@@ -69,20 +75,19 @@ public class ViewWantedController implements Initializable {
         listWanted.getSelectionModel().select(0);
     }
 
-
     private void setlabel(Person person, Suspect suspect, String crime) {
-            labelHeadName.setText(person.getFirstName().toUpperCase() + " " + person.getSurname().toUpperCase());
-            labelWanted.setText(crime);
-            labelAlias.setText(person.getFirstName().toUpperCase() + " " + person.getSurname().toUpperCase());
-            labelSSN.setText(person.getPersonalNumber().getPersonalNumber());
-            labelDateBirth.setText(AppConstant.dateOfBirth(person.getPersonalNumber().getPersonalNumber()));
-            labelGender.setText(person.getGender().toString());
-            labelEthnicity.setText(suspect.getEthnicity().toString());
-            labelHair.setText(suspect.getHairColor().toString());
-            labelHeight.setText(person.getHeight() + " cm");
-            labelWeight.setText(suspect.getWeight() + " kg");
-            labelEyes.setText(suspect.getEyeColor().toString());
-            labelThin.setText(suspect.getBuild().toString());
-            labelDescription.setText(suspect.getCharacteristic());
+        labelHeadName.setText(person.getFirstName().toUpperCase() + " " + person.getSurname().toUpperCase());
+        labelWanted.setText(crime);
+        labelAlias.setText(person.getFirstName().toUpperCase() + " " + person.getSurname().toUpperCase());
+        labelSSN.setText(person.getPersonalNumber().getPersonalNumber());
+        labelDateBirth.setText(AppConstant.dateOfBirth(person.getPersonalNumber().getPersonalNumber()));
+        labelGender.setText(person.getGender().toString());
+        labelEthnicity.setText(suspect.getEthnicity().toString());
+        labelHair.setText(suspect.getHairColor().toString());
+        labelHeight.setText(person.getHeight() + " cm");
+        labelWeight.setText(suspect.getWeight() + " kg");
+        labelEyes.setText(suspect.getEyeColor().toString());
+        labelThin.setText(suspect.getBuild().toString());
+        labelDescription.setText(suspect.getCharacteristic());
     }
 }
