473cc7dde616c27643cc9f2f33881033b15a8fe8
Soutine <jsoutine@gmail.com>
2019-05-27 20:55:42 +0200

    finalizing report crime

--- src/main/java/pust/controller/main_window/ReportCrimeController.java
+++ src/main/java/pust/controller/main_window/ReportCrimeController.java
@@ -5,12 +5,15 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
-import pust.model.enumerations.Build;
-import pust.model.enumerations.Color;
-import pust.model.enumerations.Ethnicity;
-import pust.model.enumerations.Gender;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+import pust.model.database_functionality.SelectPerson;
+import pust.model.entity.Person;
+import pust.model.enumerations.*;
 import pust.model.utility.AppConstant;
+
 import java.net.URL;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
 public class ReportCrimeController implements Initializable {
@@ -27,7 +30,7 @@ public class ReportCrimeController implements Initializable {
     @FXML
     private DatePicker crimeDatePick, reportDatePick;
     @FXML
-    private ChoiceBox<String> crimeIDBox;
+    private ChoiceBox<Crime> crimeIDBox;
     @FXML
     private ChoiceBox<Gender> notifierGenderBox;
     @FXML
@@ -43,19 +46,21 @@ public class ReportCrimeController implements Initializable {
     @FXML
     private ChoiceBox<Ethnicity> suspectEthnicityBox;
     @FXML
-    private Label pustLabel, boldLabel;
-
-    private ObservableList<String> crimeIDList = FXCollections
-            .observableArrayList("1","2","3","4");
+    private Label notifierSSNLabel, suspectSSNLabel;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         fillChoiceBoxes();
+        notifierSSNLabel.setVisible(false);
+        suspectSSNLabel.setVisible(false);
+        if (AppConstant.person != null) {
+            notifierSSNField.setText(AppConstant.person.getPersonalNumber().getPersonalNumber());
+        }
     }
 
     private void fillChoiceBoxes() {
         notifierGenderBox.getItems().setAll(Gender.values());
-        crimeIDBox.setItems(crimeIDList);
+        crimeIDBox.getItems().setAll(Crime.values());
         suspectGenderBox.getItems().setAll(Gender.values());
         suspectBuildBox.getItems().setAll(Build.values());
         suspectDescGenderBox.getItems().setAll(Gender.values());
@@ -65,17 +70,39 @@ public class ReportCrimeController implements Initializable {
     }
 
     //These control the buttons, so far only local values, will be filled by database.
-    public void notifierAutoBtnPressed(){
+    public void notifierAutoBtnPressed() {
+        String personSSN = notifierSSNField.getText().trim();
+        if (personSSN.isEmpty()) {
+            enterSSN();
+        } else if (personSSN.length() != 12 || !personSSN.matches("^[0-9]*$")) {
+            notifierSSNLabel.setVisible(true);
+        } else {
+            AppConstant.person = new SelectPerson(notifierSSNField.getText()).loadPerson();
+            if (AppConstant.person == null) {
+                AppConstant.alertBoxInformation("Person not found", String.format
+                        ("No person found in system with SSN: %s", notifierSSNField.getText()));
+                notifierSSNField.clear();
+                notifierSSNLabel.setVisible(false);
+            } else {
+                notifierSSNLabel.setVisible(false);
+                fillNotifier();
+            }
+        }
+    }
+
+    private void fillNotifier() {
         notifierFirstNameField.setText(AppConstant.person.getFirstName());
         notifierLastNameField.setText(AppConstant.person.getSurname());
-        notifierCityField.setText(AppConstant.person.getAddress().getCity());
         notifierPhoneField.setText(AppConstant.person.getPhoneNumber());
         notifierSSNField.setText(String.valueOf(AppConstant.person.getPersonalNumber().getPersonalNumber()));
         notifierStreetField.setText(AppConstant.person.getAddress().getStreet());
         notifierZIPField.setText(String.valueOf(AppConstant.person.getAddress().getZipCode()));
+        notifierCityField.setText(AppConstant.person.getAddress().getCity());
+        notifierGenderBox.setValue(Gender.valueOf(AppConstant.person.getGender().toString()));
+
     }
 
-    public void suspectAutoBtnPressed(){
+    public void suspectAutoBtnPressed() {
         suspectFirstNameField.setText("Lex");
         suspectLastNameField.setText("Luthor");
         suspectCityField.setText("Metropolis");
@@ -83,5 +110,23 @@ public class ReportCrimeController implements Initializable {
         suspectSSNField.setText("33123212");
         suspectStreetField.setText("Evil Road 5c");
         suspectZIPField.setText("546 87");
+    }
+
+    private void enterSSN() {
+        TextInputDialog dialog = new TextInputDialog();
+        Stage stage = (Stage) dialog.getDialogPane().getScene().getWindow();
+        stage.getIcons().add(new Image(this.getClass().getResource("/image/icon.png").toString()));
+        dialog.getDialogPane().getStylesheets().add(getClass().getResource("/view/basicStyleSheet.css").toExternalForm());
+        dialog.setTitle("Enter SSN");
+        dialog.setHeaderText(null);
+        dialog.setContentText("SSN:");
+        Optional<String> result = dialog.showAndWait();
+
+        if (result.isPresent() && result.get().length() == 12) {
+            notifierSSNField.setText(result.get());
+            notifierAutoBtnPressed();
+        } else if (result.isPresent() && result.get().length() != 12) {
+            AppConstant.alertBoxInformation("Format", "SSN must have 12 characters");
+        }
     }
 }
